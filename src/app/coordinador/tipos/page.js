80d1af974b59fe28f09eb4225710dc0736833b20
alez-2210 <a20201403@pucp.edu.pe>
2024-05-03 22:28:00 -0500

    Árbol de pantallas listo

---
+++ src/app/coordinador/tipos/page.js
@@ -0,0 +1,29 @@
+"use client";
+//prueba
+import LayoutComponent from "@/components/LayoutComponent";
+import { useEffect, useState } from "react";
+import { Button, Flex, Typography, Modal, Input } from "antd";
+import axios from "axios";
+import { coordinadorItems } from "@/utils/menuItems";
+
+const { Title } = Typography;
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const get = async () => {
+    setIsLoading(true);
+  };
+
+  useEffect(() => {
+    get();
+  }, []);
+
+  return (
+    <main style={{ height: "100vh" }}>
+      <LayoutComponent siderItems={coordinadorItems}>
+      </LayoutComponent>
+    </main>
+  );
+}
+
--- src/utils/menuItems.js
+++ src/utils/menuItems.js
@@ -1,6 +1,9 @@
 import {
   IconSchool, IconUsersGroup, IconCalendarWeek, IconList, IconBook, IconNotes, IconSquarePlus,
-  IconHeartHandshake, IconUserPlus, IconFirstAidKit, IconFileTypeDoc, IconReportAnalytics
+  IconHeartHandshake, IconUserPlus, IconFirstAidKit, IconFileTypeDoc, IconReportAnalytics,
+  IconUsers,
+  IconBuildingBank,
+  IconBuilding
 } from "@tabler/icons-react";
 
 function getItem(label, key, icon, link, visible, children, type) {
@@ -25,13 +28,28 @@ export const tutorItems = [
 ];
 
 export const coordinadorItems = [
-  getItem("Usuarios", "1", <IconUsersGroup size={20}/>, "coordinador/usuarios", true),
-  getItem("Tipos de Tutoría", "2", <IconSchool size={20} />, "coordinador/tipos", true),
-  getItem("Asignación Individual Tipo de Tutoría", "3", <IconSquarePlus size={20} />, "coordinador/astipo", true),
-  getItem("Asignación Masiva Tipo de Tutoría", "4", <IconSquarePlus size={20} />, "coordinador/astipomas", true),
-  getItem("Asignar Tutor", "5", <IconHeartHandshake size={20} />, "coordinador/astutor", true),
-  getItem("Registro de Usuarios", "6", <IconUserPlus size={20} />, "coordinador/regusuarios", true),
-  getItem("Unidades de Apoyo", "7", <IconFirstAidKit size={20} />, "coordinador/uapoyo", true),
-  getItem("Agregar Documentos", "8", <IconFileTypeDoc size={20} />, "coordinador/docs", true),
-  getItem("Reportes", "9", <IconReportAnalytics size={20} />, "coordinador/reportes", false),
+  getItem("Usuarios", "1", <IconUsersGroup size={20}/>, "/coordinador/usuarios", true),
+  getItem("Tipos de Tutoría", "2", <IconSchool size={20} />, "/coordinador/tipos", true),
+  getItem("Asignación Individual Tipo de Tutoría", "3", <IconSquarePlus size={20} />, "/coordinador/astipo", true),
+  getItem("Asignación Masiva Tipo de Tutoría", "4", <IconSquarePlus size={20} />, "/coordinador/astipomas", true),
+  getItem("Asignar Tutor", "5", <IconHeartHandshake size={20} />, "/coordinador/astutor", true),
+  getItem("Registro de Usuarios", "6", <IconUserPlus size={20} />, "/coordinador/regusuarios", true),
+  getItem("Unidades de Apoyo", "7", <IconFirstAidKit size={20} />, "/coordinador/uapoyo", true),
+  getItem("Agregar Documentos", "8", <IconFileTypeDoc size={20} />, "/coordinador/docs", true),
+  getItem("Reportes", "9", <IconReportAnalytics size={20} />, "/coordinador/reportes", true),
+];
+
+export const alumnoItems = [
+  getItem("Lista de tutores", "1", <IconUsers size={20}/>, "/alumno/tutores", true),
+  getItem("Horario de citas", "2", <IconCalendarWeek size={20} />, "/alumno/horario", true),
+  getItem("Lista de citas", "3", <IconList size={20} />, "/alumno/citas", true),
+  getItem("Encuestas", "4", <IconNotes size={20} />, "/alumno/encuestas", true),
+];
+
+export const adminItems = [
+  getItem("Institucion", "1", <IconBuildingBank size={20}/>, "/admin/institucion", true),
+  getItem("Facultad", "2", <IconBuilding size={20} />, "/admin/facultad", true),
+  getItem("Programa", "3", <IconSchool size={20} />, "/admin/programa", true),
+  getItem("Roles", "4", <IconUsersGroup size={20} />, "/admin/roles", true),
+  getItem("Registro de Usuarios", "5", <IconNotes size={20} />, "/admin/regusuarios", true),
 ];
