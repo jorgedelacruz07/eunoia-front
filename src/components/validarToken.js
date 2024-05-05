2f13c6a398e19c31205d1e2e545f186b604968af
xbreadze <129798313+xbreadze@users.noreply.github.com>
2024-05-05 00:52:13 -0500

    LoginRedirige

--- src/components/validarToken.js
+++ src/components/validarToken.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 // Función para validar el token en el backend
 export default async function validaToken(email) {
     try {
-        debugger
         // Enviar el token al backend
         const response = await axios.post("http://localhost:8080/usuarioApi/validarUsuario", {
             correo: email,
@@ -16,14 +15,41 @@ export default async function validaToken(email) {
         );
 
         // Verificar el resultado de la validación
-        if (response.status === 200) {
-            console.log('Usuario Valido');
+        if (response.data === 'Tutor') {
+            console.log('Usuario Tutor');
             // Aquí puedes realizar acciones adicionales si el token es válido
-            return true; // Devuelve true si el token es válido
+            alert("usuario Tutor");
+            return '/tutor'; // Devuelve true si el token es válido
         } else {
-            console.error('Usuario inválido');
-            // Aquí puedes manejar el caso en el que el token no es válido
-            return false; // Devuelve false si el token no es válido
+            if(response.data == 'Alumno'){
+                console.error('Usuario Alumno');
+                // Aquí puedes manejar el caso en el que el token no es válido
+                alert("usuario Alumno");
+                return '/alumno'; // Devuelve false si el token no es válido
+            }
+            else{
+                if(response.data == 'Coordinador'){
+                    console.error('Usuario Coordinador');
+                    // Aquí puedes manejar el caso en el que el token no es válido
+                    alert("usuario Coordinador");
+                    return '/coordinador'; // Devuelve false si el token no es válido
+                }
+                else{
+                    if(response.data == 'Administrador'){
+                        console.error('Usuario Administrador');
+                        // Aquí puedes manejar el caso en el que el token no es válido
+                        alert("usuario Administrador");
+                        return '/admin'; // Devuelve false si el token no es válido
+                    }
+                    else{
+                        console.error('Usuario no encontrado');
+                        // Aquí puedes manejar el caso en el que el token no es válido
+                        alert("usuario no valido");
+                        return '/error'; // Devuelve false si el token no es válido
+                    }
+                }
+            }
+            
         }
     } catch (error) {
         console.error('Error al validar el token:', error);
