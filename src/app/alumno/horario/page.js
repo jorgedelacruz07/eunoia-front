35b514bd2f2de889ca26c4a62a04ccef24671a11
darkmoon09032 <a20191810@pucp.edu.pe>
2024-05-09 18:22:51 -0500

    confirmacion3

--- src/app/alumno/horario/page.js
+++ src/app/alumno/horario/page.js
@@ -1,29 +1,164 @@
 "use client";
 //prueba
 import LayoutComponent from "@/components/LayoutComponent";
+
 import { useEffect, useState } from "react";
-import { Button, Flex, Typography, Modal, Input } from "antd";
+import { Button, Flex, Typography, Modal, Input ,Select} from "antd";
 import axios from "axios";
 import { alumnoItems } from "@/utils/menuItems";
-
+import {Calendar } from "./Calendario"
 const { Title } = Typography;
+import  "./horario.css"
+import { MinusCircleFilled } from '@ant-design/icons';
 
 export default function Home() {
+  const [disponibilidad, setDisponibilidad] = useState([]);
+  const [bloquesDisponibilidad, setBloqueDisponibilidad] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [filtro, setFiltro] = useState("");
+  const [tutores,setTutores] = useState([]);
+  let id ;
+  useEffect(() => {
+    // Leer los parámetros de la URL
+    const params = new URLSearchParams(window.location.search);
+    ;
+    id = params.get('id')
+    // Hacer algo con el ID (por ejemplo, imprimirlo en la consola)
+    console.log('ID del tutor:', id);
+    
+  }, []); // Ejecutar solo una vez al cargar la página
 
   const get = async () => {
     setIsLoading(true);
   };
 
-  useEffect(() => {
-    get();
-  }, []);
+  const handleListarDisponibilidad = async(idTutor)=>{
+    setIsLoading(true);
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/disponibilidadApi/listarDisponibilidadPorTutor/${idTutor}`
+      );
+      console.log(response.data);
+      setDisponibilidad(response.data);
+    } catch (error) {
+      console.error("Error al obtener datos de la API:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
+  const handlerElementosPorDisponibilidad = async (idDisponibilidad) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/bloqueDisponibilidadApi/listarBloqueDisponibilidadPorDisponibilidad/${idDisponibilidad}`
+      );
+      console.log(response.data);
+      setBloqueDisponibilidad(response.data);
+      return response.data; // Devuelve la lista de elementos
+    } catch (error) {
+      console.error("Error al obtener datos de la API: ", error);
+      return []; // Devuelve un arreglo vacÃo en caso de error
+    }
+  };
+ 
+  
+  useEffect(() => {
+    const idTutor = id;
+    handleListarDisponibilidad(idTutor);
+  }, [id,filtro]);
+
+
+  let idDispo;
+  disponibilidad.forEach((elemento) => {
+     idDispo= elemento.idDisponibilidad;
+    
+  });
+  
+  console.log(idDispo);
+
+  
+ 
+
+  const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+  const startDate = new Date();
+  const monthName = months[startDate.getMonth()]; // Obtén el nombre del mes actual
+  const year = startDate.getFullYear();
+
+const formattedDate = `${monthName} ${year}`;
+  useEffect(() => { 
+    
+    handlerElementosPorDisponibilidad(idDispo);
+  }, [idDispo,filtro]);
+
+  function handleChange(value) {   
+    ;    
+  }
   return (
     <main style={{ height: "100vh" }}>
       <LayoutComponent siderItems={alumnoItems}>
+        <div>
+          <h1 className="titulo-principal">Agendar citas</h1>
+        </div>
+        <div className="fila-1">
+          <div>
+            <p className="fecha"> {monthName} {year}</p>
+          </div>
+          <div>
+            <Select
+                defaultValue={tutores[0]}
+                placeholder="Seleccione el tema" // Establecer el valor predeterminado, por ejemplo, el primer elemento del arreglo.
+                style={{ 
+                  width: "359px",
+                  height: "38px",
+                  border: "2px solid #0663bd",
+                  borderRadius: "10px",
+                  fontSize: "20px",
+                  //colorBgContainer : "#ffffff",
+                  //marginLeft: "500px"
+                 }}
+                 
+                 onChange={handleChange}
+                >
+                
+                {tutores.map((opcion, index) => (
+                  <Option key={index} value={opcion.idTipoTutoria}>
+                    {opcion.nombre}
+                  </Option>
+                ))}
+              </Select>
+          </div>
+        </div>
+        <div className="fila-2">
+        
+          <ul className="horizontal-list">
+          <MinusCircleFilled 
+            style={{
+              color:"#a7eeb8",
+              marginRight: "10px"
+            }}
+          />
+            <li className="palabra"> Disponible </li>
+            <MinusCircleFilled style={{
+              color:"#D4C095",
+              marginRight: "10px"
+            }}/>
+            <li className="palabra"> Ocupado </li>
+            <MinusCircleFilled style={{
+              color:"#B9D5EF",
+              marginRight: "10px"
+            }}/>
+            <li className="palabra"> Reservado</li>
+          </ul>
+          <button className="boton">Solicitar tutoría Fija</button>
+        </div>
+        <div className="calendario-contenedor">
+          <Calendar bloques={bloquesDisponibilidad}
+          
+       />
+        </div>
       </LayoutComponent>
     </main>
+
   );
 }
-
--- src/app/alumno/tutores/page.js
+++ src/app/alumno/tutores/page.js
@@ -2,26 +2,231 @@
 //prueba
 import LayoutComponent from "@/components/LayoutComponent";
 import { useEffect, useState } from "react";
-import { Button, Flex, Typography, Modal, Input } from "antd";
+import { Button, Flex, Typography, Modal, Input ,Select,Text } from "antd";
 import axios from "axios";
 import { alumnoItems } from "@/utils/menuItems";
-
+import { TutorCard } from "./cardTutor";
 const { Title } = Typography;
+import {  cardTutor } from './cardTutor.css'
+import { Divider } from "antd";
+import "./app.css";
+import { userAgent } from "next/server";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false);
+  const [tutores,setTutores] = useState([]);
+  const [tiposTutoria,setTiposTutoria] = useState([]);
+  const [tutoresPrueba,setTutoresPrueba] = useState([]);
+  const [texto,setTexto ] = useState("");
+  const [tiposTutoriaAlumno,setTiposTutoriaAlumno] = useState([]);
+  const [tutoriaSelccionado,setTutoriaSeleccionado] = useState(-1)
+  
 
-  const get = async () => {
+
+
+  const handlerListarTutores = async (id,nombre,idTipo) => {
     setIsLoading(true);
+    try{
+      const response = await axios.get(
+        `http://localhost:8080/tutorApi/listarTutorPorAlumno/${id}/${nombre}/${idTipo}`
+      );
+      setTutores(response.data);
+      
+    } catch(error){
+      console.error("Error al obtener datos de la API: listar tutores", error);
+    } finally{
+      setIsLoading(false);
+    }
   };
 
+  const handlerListarTemasPorTutor = async (idUsuario,idTutor) => {
+    setIsLoading(true);
+    try{
+      const response = await axios.get(
+        `http://localhost:8080/tipoTutoriaApi/listarTiposTutoriaCompatiblesXTutor/${idUsuario}/${idTutor}`
+      );
+      setTiposTutoria(response.data);
+      return response.data;
+     
+    } catch(error){
+      console.error("Error al obtener datos de la API: Tipors tutoria", error);
+    } finally{
+      setIsLoading(false);
+    }
+  };
+
+  const handlerListarTutoriasPorAlumno = async (idUsuario) => {
+    setIsLoading(true);
+    try{
+      const response = await axios.get(
+        `http://localhost:8080/tipoTutoriaApi/listarTiposTutoriaXAlumno/${idUsuario}`
+      );
+      setTiposTutoriaAlumno(response.data);
+      return response.data;
+     
+    } catch(error){
+      console.error("Error al obtener datos de la API: Tipos tutoria por alumno", error);
+    } finally{
+      setIsLoading(false);
+    }
+  };
+
+ 
+  const llenarTiposTutoria = async (tutoresParametro) => {
+    const nuevosTutores = await Promise.all(
+      tutoresParametro.map(async (elemento) => {
+        
+        const tiposTutoriaA = await handlerListarTemasPorTutor(1, elemento.persona.id);
+        const tutor = {
+          id: elemento.persona.id,
+          tiposDeTutoria: tiposTutoriaA,
+          codigo : elemento.codigo,
+          nombre : elemento.persona.nombre,
+          apellidoPaterno : elemento.persona.apellidoPaterno,
+          apellidoMaterno : elemento.persona.apellidoMaterno,
+          foto :elemento.foto,
+          
+        };
+        return tutor;
+      })
+    );
+    setTutoresPrueba(nuevosTutores);
+  };
+
+  
+  
+
   useEffect(() => {
-    get();
-  }, []);
+      
+    const idUsuario = 1; // Debes proporcionar el id del alumno aquí   
+    handlerListarTutoriasPorAlumno(idUsuario);
+    
+  }, [])
+
+ 
+  useEffect(() => {
+      debugger
+      const idAlumno = 1;
+      
+      if ( texto === null || texto === ''){
+        handlerListarTutores(idAlumno, '%20' ,tutoriaSelccionado );
+      }
+      else{
+        handlerListarTutores(idAlumno, texto ,tutoriaSelccionado );
+      }// Debes proporcionar el id del alumno aquí   
+      
+      console.log(texto);
+      console.log(tutoriaSelccionado);
+      
+  }, [texto,tutoriaSelccionado])
+
+
+    
+
+  useEffect(() => {
+      const idAlumno = 1;
+      const textoInicial = '%20'
+      handlerListarTutores(idAlumno,textoInicial,-1 );
+       // Suponiendo que necesitas realizar una acción similar a esta
+  }, []);  
+    
+  
+  useEffect(() => {
+    
+    llenarTiposTutoria(tutores);
+    
+  }, [tutores]);
+  
+  function handleChange(value,txt) {   
+    setTutoriaSeleccionado(value);
+    //handlerListarTutores(1,' ',value);
+    console.log(tutoriaSelccionado);    
+  }
+
+  console.log(tiposTutoria);
 
   return (
     <main style={{ height: "100vh" }}>
       <LayoutComponent siderItems={alumnoItems}>
+        <div className="titulo-principal">
+          <h1>Lista de tutores</h1>
+        </div>
+        <div>
+          <Divider orientation="left" 
+           style={{ color: '#727272',
+                  fontFamily: "Nunito",
+                  fontWeight: "light",
+                  fontSize: '18px' ,  
+                  colorBorderSecondary : 'rgba(114, 114,144, 10)'}}>Tutores asignados
+           </Divider>
+        </div>
+        <div className="otros-tutores"  >
+          <Divider orientation="left" 
+                  style={{ color: '#727272',
+                  fontFamily: "Nunito",
+                  fontSize: '18px' }}>Otros tutores</Divider>
+          <div className="buscadores">
+            <Input
+              placeholder="Buscar tutor por nombre"
+              value=  {texto}   
+              onChange={(e) => {
+                const valor = e.target.value;
+                setTexto(valor === null ? '%20' : valor);              
+                //setTexto(e.target.value);
+              }}
+              //onPressEnter={handlerListarTutores(1,"a",-1)}
+              style={{
+                borderRadius: "20px",
+                border: "2px solid #0663bd",
+                padding: "10px",
+                fontSize: "20px",
+                width: "670px",
+                height: "50px",
+                marginLeft : "60px", 
+                marginRight : "170px"   
+              }}
+            />
+            <div className="dropdownContainerStyle">
+           
+            <Select
+                defaultValue={tiposTutoriaAlumno[0]}
+                placeholder="Seleccione el tema" // Establecer el valor predeterminado, por ejemplo, el primer elemento del arreglo.
+                style={{ 
+                  width: "359px",
+                  height: "50px",
+                  border: "2px solid #0663bd",
+                  borderRadius: "10px",
+                  fontSize: "20px",
+                  //colorBgContainer : "#ffffff",
+                  //marginLeft: "500px"
+                 }}
+                 
+                 onChange={handleChange}
+                >
+                <Option value={-1} >Todos</Option>
+                {tiposTutoriaAlumno.map((opcion, index) => (
+                  <Option key={index} value={opcion.idTipoTutoria}>
+                    {opcion.nombre}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+          </div>
+        
+        </div>
+        {tutoresPrueba.map((tutor, index) => (
+          
+          <TutorCard 
+            key={index}
+            id = {tutor.id}
+            codigo={tutor.codigo}
+            nombre={tutor.nombre}
+            apellidoMaterno={tutor.apellidoMaterno}
+            apellidoPaterno={tutor.apellidoPaterno}
+            foto={tutor.foto}
+            listaTiposTutoria={tutor.tiposDeTutoria}
+          />
+        ))}
       </LayoutComponent>
     </main>
   );
